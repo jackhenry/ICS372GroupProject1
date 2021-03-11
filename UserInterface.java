ba0c44570c9f36827af384aff1716743aebe044d
luannvm09 <70990811+luannvm09@users.noreply.github.com>
2021-03-10 20:04:20 -0600

    Add files via upload
    Upload all 10 classes I have

---
+++ UserInterface.java
@@ -0,0 +1,216 @@
+package ui;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *  This is the UserInterface Class that will be used
+ *  to display information to user, and receive commands.
+ *  It follows Singleton Pattern
+ */
+public class UserInterface {
+	private static UserInterface userInterface;
+	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private static final int EXIT = 0;
+	private static final int ADD_MEMBER = 1;
+	private static final int REMOVE_MEMBER = 2;
+	private static final int ADD_PRODUCT = 3;
+	private static final int CHECKOUT = 4;
+	private static final int PROCESS_SHIPMENT = 5;
+	private static final int CHANGE_PRODUCT_PRICE = 6;
+	private static final int PRODUCT_INFO = 7;
+	private static final int MEMBER_INFO = 8;
+	private static final int PRINT_TRANSACTIONS = 9;
+	private static final int OUTSTANDING_ORDERS = 10;
+	private static final int LIST_MEMBERS = 11;
+	private static final int LIST_PRODUCTS = 12;
+	private static final int SAVE = 13;
+	private static final int HELP = 14;
+
+	/**
+	 * private constructor for singleton pattern
+	 */
+	private UserInterface() {
+		// FIXME check if there is saved data
+	}
+
+	// This is Luan 
+	
+	
+	/**
+	 * instance method that returns existing UI if it is already
+	 * created, if not it returns a new UI.
+	 */
+	public static UserInterface instance() {
+		if(userInterface == null) {
+			return userInterface = new UserInterface();
+		}
+		else {
+			return userInterface;
+		}
+	}
+
+	/**
+	 * This is a method to get the list of input commands
+	 * @returns menu - used in help command
+	 */
+	private String showMenu() {
+		String menu = "Make a selection, enter: \n"
+				+ ADD_MEMBER + ") Enroll a member\n"
+				+ REMOVE_MEMBER + ") Remove a member\n"
+				+ ADD_PRODUCT + ") Add a product\n"
+				+ CHECKOUT + ") Check out a member's items\n"
+				+ PROCESS_SHIPMENT + ") Process a shipment\n"
+				+ CHANGE_PRODUCT_PRICE + ") Change the price of a product\n"
+				+ PRODUCT_INFO +") Retrieve product info\n"
+				+ MEMBER_INFO + ") Retrieve member info\n"
+				+ PRINT_TRANSACTIONS + ") Print transactions\n"
+				+ OUTSTANDING_ORDERS + ") List all outstanding orders\n"
+				+ LIST_MEMBERS + ") List all members with member info\n"
+				+ LIST_PRODUCTS +") List all products with product info\n"
+				+ SAVE + ") Save\n"
+				+ HELP + ") Help \n"
+				+ "Press " + EXIT + " at any time to quit the application";
+		return menu;
+	}
+
+	/**
+	 * a method to get the first word entered from user.
+	 * @param message - desired message to instruction
+	 * @return the first word of the user input
+	 */
+	private String getFirstWord(String message) {
+		do {
+			try {
+				System.out.print(message);
+				String line = reader.readLine();
+				String[] words = line.split("\\s");
+				return words[0];
+			}catch(IOException ioe) {
+				System.exit(0);
+			}
+		} while(true);
+	}
+
+	/**
+	 *  returns a line of text entered and shows user a prompt
+	 * @param message - command given
+	 * @return String - answer provided by user
+	 */
+	private String getStringInput(String message) {
+		do {
+			try {
+				System.out.print(message);
+				String line = reader.readLine();
+				return line;
+			}catch(IOException ioe) {
+				System.exit(0);
+			}
+		} while(true);
+	}
+
+	/**
+	 * A method to get an Int value from user input
+	 * @param message
+	 * @return int value from String input
+	 */
+	private int getIntegerInput(String message) {
+		do {
+			try {
+				String rawUserInput = getFirstWord(message);
+				Integer userIntegerInput = Integer.valueOf(rawUserInput);
+				return userIntegerInput.intValue();
+			}catch(NumberFormatException nfe) {
+				System.out.println("Input must be a number 0 - 14\n"
+						+ "Enter " + HELP + " for help");
+			}
+		}while(true);
+	}
+
+	/**
+	 *  This method catches user inputs, and relies
+	 *  on getIntegerInput and getFirstWordInput to process
+	 *  inputs
+	 *  @param none
+	 *  @return void
+	 */
+	public void showUserInterface() {
+		System.out.println(showMenu());
+		boolean continueApplication = true;
+		while(continueApplication) {
+			try {
+				int userChoice = getIntegerInput("Enter: ");
+				switch(userChoice) {
+				case(EXIT):
+					System.out.println("Program Succesfully close");
+				System.exit(0);
+				case (ADD_MEMBER):
+					// FIXME
+					//enroll a member
+					break;
+				case(REMOVE_MEMBER):
+					// FIXME
+					//remove a member
+					break;
+				case(ADD_PRODUCT):
+					// FIXME
+					//add a product
+					break;
+				case(CHECKOUT):
+					// FIXME
+					//check out a members products
+					break;
+				case(PROCESS_SHIPMENT):
+					// FIXME
+					//process a shipment
+					break;
+				case(CHANGE_PRODUCT_PRICE):
+					// FIXME
+					//change product price
+					break;
+				case(PRODUCT_INFO):
+					// FIXME
+					//retrieve prod info
+					break;
+				case(MEMBER_INFO):
+					// FIXME
+					//retrieve member info
+					break;
+				case(PRINT_TRANSACTIONS):
+					// FIXME
+					//print transactions
+					break;
+				case(OUTSTANDING_ORDERS):
+					// FIXME
+					//list outstanding orders
+					break;
+				case(LIST_PRODUCTS):
+					// FIXME
+					//list member and member info
+					break;
+				case(LIST_MEMBERS):
+					// FIXME
+					//list prods and prod info
+					break;
+				case(SAVE):
+					// FIXME
+					//save
+					break;
+				case(HELP):
+					System.out.println(showMenu());
+				break;
+				default:
+					System.out.println("Invalid entry. Enter " + HELP + " for help");
+				}
+			} catch (Exception e) {
+				System.out.println("Unexpected Error. Restart Program.");
+				System.exit(0);
+			}
+		}
+	}
+}
+
+
+
